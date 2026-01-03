ff06d56290a2e0dced4cf54cfd898217aa72e206
Nantipat J <nantipat4@gmail.com>
2026-01-03 03:19:57 +0000

    add new functon

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is my fitst script
 print("hello world")
 print("I am using github with posit.cloud")
+
+def hello():
+  name = input("What's your name? :")
+  print(f"Hello {name}!")
+  
+# test function
+hello()
